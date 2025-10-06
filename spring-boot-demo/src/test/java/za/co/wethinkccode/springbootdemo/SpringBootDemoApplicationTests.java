ab61f25532204446d313eb7b9e642e4d655a90d1
bamazom <bamazomjhb024@student.wethinkcode.co.za>
2025-10-06 21:21:21 +0200

    spring boot hello world

---
+++ spring-boot-demo/src/test/java/za/co/wethinkccode/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.wethinkccode.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
